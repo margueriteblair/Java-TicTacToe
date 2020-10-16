2a6ca5247a23a8625d77d367c43d74d89187b294
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-15 23:00:59 -0400

    created random position generated for the cpu

--- src/com/gametut/Main.java
+++ src/com/gametut/Main.java
@@ -1,5 +1,8 @@
 package com.gametut;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,13 +13,73 @@ public class Main {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}}; //three rows
 
-        for (char [] row : gameBoard ) {
+        //this just prints out the game board
+        printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement: 1-9");
+
+        int position = scan.nextInt();
+        System.out.println(position);
+
+        placePiece(gameBoard, position, "player");
+
+        Random random = new Random();
+        int cpuPos = random.nextInt(9) + 1;
+        placePiece(gameBoard, cpuPos, "comp");
+        printGameBoard(gameBoard);
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
             for (char c : row) {
                 System.out.print(c);
             }
             System.out.println();
         }
     }
+
+    public static void placePiece(char[][] gameBoard, int position, String user) {
+        char symbol = 'X';
+        if (user.equals("player")) {
+            symbol = 'X';
+        } else if (user.equals("comp")) {
+            symbol = '0';
+        }
+
+
+        switch(position) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+        }
+
+    }
+
 }
 
 
